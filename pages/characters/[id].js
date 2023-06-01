92892900dcddd4a1aacd8a1c381ce0fe6da8aad8
Daniel Schuetzle <102250825+Danielschuetzle@users.noreply.github.com>
2023-06-01 16:47:08 +0200

    comments added

--- pages/characters/[id].js
+++ pages/characters/[id].js
@@ -3,35 +3,44 @@ import useSWR from "swr";
 import Card from "../../components/Card";
 import Layout from "../../components/Layout";
 
+// Fetcher function that fetches data from API and handles errors
 const fetcher = async (url) => {
   const res = await fetch(url);
 
+  // If there's an error, throw an error with status
   if (!res.ok) {
     const error = new Error("An error occurred while fetching the data.");
     error.status = res.status;
     throw error;
   }
 
+  // Return fetched data
   return res.json();
 };
 
+// Character component
 export default function Character() {
+  // Access router object to get id from query parameters
   const router = useRouter();
-  const { id } = router.query; // Access id from router.query
+  const { id } = router.query;
 
+  // Use SWR hook to fetch data using id from query parameters
   const { data, error } = useSWR(
     id ? `https://swapi.dev/api/people/${id}/` : null,
     fetcher
   );
 
+  // If there's an error, return error message
   if (error) {
     return <div>Error: {error.message}</div>;
   }
 
+  // While data is being fetched, display loading message
   if (!data) {
     return <div>Loading...</div>;
   }
 
+  // Render Card component with fetched data
   return (
     <Layout>
       <Card
